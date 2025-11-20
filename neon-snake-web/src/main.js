fb7a06867a993500931895eea2296c63ceef6ccc
Skogmartin <skogmartin@gmail.com>
2025-11-20 20:59:58 +0100

    Make game responsive to screen size

--- neon-snake-web/src/main.js
+++ neon-snake-web/src/main.js
@@ -3,12 +3,12 @@ import HighScoreScene from './scenes/HighScoreScene.js';
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     backgroundColor: '#0f0f19',
     parent: 'game-container',
     scale: {
-        mode: Phaser.Scale.FIT,
+        mode: Phaser.Scale.RESIZE,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
     physics: {
